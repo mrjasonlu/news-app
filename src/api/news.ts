4131e7b400fa2ac40dc15bf641330fb226a9d897
Jason Lu <jasonlu@live.com.au>
2023-11-23 09:28:44 +1100

    remove comments

--- src/api/news.ts
+++ src/api/news.ts
@@ -3,19 +3,8 @@ import mockNewsData from './mockNewsData';
 import { NewsArticleT } from 'pages/home/components/news/types';
 
 export async function searchArticles(query: string): Promise<NewsArticleT[]> {
+  // url used for fetching real api calls for future
   const url = NEWS_API_URL;
-  // url used for fetching real api calls
-  /* 
-  status: 'ok',
-  userTier: 'developer',
-  total: 29731,
-  startIndex: 1,
-  pageSize: 10,
-  currentPage: 1,
-  pages: 2974,
-  orderBy: 'relevance',
-  results:
-  */
 
   return new Promise((resolve) => {
     if (!query) {
